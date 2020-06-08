69493c629853b9c7a4297dd23d6775a8c1b8e8d9
tedsmoore <tedsmoore@gmail.com>
2020-06-08 16:07:09 -0400

    team name and keeper status

--- league_history/views.py
+++ league_history/views.py
@@ -12,7 +12,9 @@ def index(request, pick_id):
         {
             'num': idx + 1,
             'player': pick['first_name'] + ' ' + pick['last_name'],
-            'selected': is_picked(draft_picks, pick['id'], int(pick_id))
+            'selected': is_picked(draft_picks, pick['id'], int(pick_id)),
+            'keeper': is_keeper(draft_picks, pick['id']),
+            'drafted_by': drafted_by(draft_picks, pick['id'])
         }
         for idx, pick in player_ranks.iterrows()
     ]
@@ -30,3 +32,24 @@ def is_picked(draft_picks, player_id, current_pick):
 
     pick_num = 10 * (pick.round_num - 1) + pick.round_pick
     return pick_num < current_pick
+
+
+def is_keeper(draft_picks, player_id):
+    try:
+        pick = draft_picks.get(player_id=player_id)
+    except ObjectDoesNotExist:
+        return ""
+
+    if pick.is_keeper:
+        return "YES"
+    else:
+        return ""
+
+
+def drafted_by(draft_picks, player_id):
+    try:
+        pick = draft_picks.get(player_id=player_id)
+    except ObjectDoesNotExist:
+        return "N/A"
+
+    return Team.objects.get(id=pick.team_id)
